70c55af5e3ce834481540d884a06ac986aa592df
Carlos Quiroz <carlos.m.quiroz@gmail.com>
2013-11-18 17:45:17 -0300

    WIP

--- simulations/src/main/scala/simulations/EpidemySimulator.scala
+++ simulations/src/main/scala/simulations/EpidemySimulator.scala
@@ -1,6 +1,6 @@
 package simulations
 
-import math.random
+import math.{random, min, max}
 
 class EpidemySimulator extends Simulator {
 
@@ -16,7 +16,10 @@ class EpidemySimulator extends Simulator {
 
   import SimConfig._
 
-  val persons: List[Person] = (for {id <- 0 to population} yield new Person(id)).toList
+  val persons: List[Person] = (for {id <- 0 until population} yield new Person(id)).toList
+  persons.take(population / 100).foreach { p =>
+    p.infected = true
+  }
 
   class Person (val id: Int) {
     var infected = false
@@ -28,6 +31,46 @@ class EpidemySimulator extends Simulator {
     var row: Int = randomBelow(roomRows)
     var col: Int = randomBelow(roomColumns)
 
-    afterDelay(5) {println("Abc")}
+    def move() {
+      if (!dead) {
+        val (newRow, newCol) = randomBelow(3) match {
+          case 0 => (if (row == 0) roomRows else row - 1, col) // up
+          case 1 => (if (row == roomRows) 0 else row + 1, col) // down
+          case 2 => (row, if (col == 0) roomColumns else col - 1) // left
+          case 3 => (row, if (col == roomColumns) 0 else col + 1) // right
+        }
+        val canMove = persons.forall { p =>
+          (p.col != newCol && p.row != newRow) || (!p.dead && !p.infected)
+        }
+        val anyInfectious = persons.exists { p =>
+          p.col == newCol && p.row == newRow && p.infected
+        }
+        if (anyInfectious) {
+          infected = randomBelow(100) < 40
+          if (infected) {
+            afterDelay(6) {
+              sick = true
+            }
+            afterDelay(14) {
+              dead = randomBelow(100) < 25
+            }
+            afterDelay(16) {
+              immune = true
+            }
+          }
+        }
+        if (canMove) {
+          row = newRow
+          col = newCol
+        }
+        afterDelay(randomBelow(5)) {
+          move()
+        }
+      }
+    }
+
+    afterDelay(randomBelow(5)) {
+      move()
+    }
   }
 }
